29749b5299062fb0f871ffdd915c22af19fe0982
Dawnfrosty <88573246+Dawnfrosty@users.noreply.github.com>
2021-08-07 19:30:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/BiBYkaukOdlDZPQxaWzmtG', 'https://chat.whatsapp.com/CgzjytokmxyF3tOfRs0x1d']
+global.owner = ['6282297563380', '6282297563380', '6282297563380']// Put your number here
+global.mods = ['082297563380'] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'c2e1f9861a763992',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais'
+}
+
+// Sticker WM
+global.packname = 'I Hope you for the best'
+global.author = 'YveBot'
+
+global.multiplier = 39 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
